42d50d7c72020f7f89974a5294b590c75cc3378c
Betsy Haibel <betsy.haibel@gmail.com>
2016-01-23 13:06:34 -0500

    extract jade mixins to separate file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp  = require("gulp");
 var gutil = require("gulp-util");
 var jade  = require("gulp-jade");
-
+var del   = require("del");
 
 var paths = {
   "src": {
-    "templates": "./src/templates/**/*.jade"
+    "templates": "./src/templates/**/[!_]*.jade"
   },
   "build": {
     "root": "./build"
@@ -28,11 +28,11 @@ var compiledTemplates = function() {
            .pipe(jade({locals: templateData, pretty: true}));
 }
 
-gulp.task('build:templates', function() {
+gulp.task('build:templates', ['clean:build:templates'], function() {
   compiledTemplates().pipe(gulp.dest(paths.build.root));
 });
 
-gulp.task('dist:templates', function() {
+gulp.task('dist:templates', ['clean:dist:templates'], function() {
   compiledTemplates().pipe(gulp.dest(paths.dist.root));
 });
 
@@ -40,6 +40,14 @@ gulp.task('watch', function() {
   gulp.watch(paths.src.templates, ['build:templates']);
   gulp.watch(paths.data.meta,     ['build:templates']);
   gulp.watch(paths.data.people,   ['build:templates']);
+});
+
+gulp.task('clean:build:templates', function() {
+  del(paths.build.root + '/**/*.html');
+});
+
+gulp.task('clean:dist:templates', function() {
+  del(paths.dist.root + '/**/*.html');
 });
 
 gulp.task('build', ['build:templates']);
