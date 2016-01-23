6f74940c5335f1a9ba2a006bc4e2505aadeb623b
Betsy Haibel <betsy.haibel@gmail.com>
2016-01-23 11:32:41 -0500

    include data in compiled templates

--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,36 @@ var paths = {
   },
   "dist": {
     "root": "./dist"
+  },
+  "data": {
+    "meta":   "./data/meta.json",
+    "people": "./data/people.json"
   }
 };
 
+var compiledTemplates = function() {
+  var templateData = {};
+  templateData.meta   = require(paths.data.meta);
+  templateData.people = require(paths.data.people);
+
+  return gulp.src(paths.src.templates)
+           .pipe(jade({locals: templateData, pretty: true}));
+}
+
 gulp.task('build:templates', function() {
-  gulp.src(paths.src.templates)
-    .pipe(jade())
-    .pipe(gulp.dest(paths.build.root));
+  compiledTemplates().pipe(gulp.dest(paths.build.root));
 });
 
 gulp.task('dist:templates', function() {
-  gulp.src(paths.src.templates)
-    .pipe(jade())
-    .pipe(gulp.dest(paths.dist.root));
+  compiledTemplates().pipe(gulp.dest(paths.dist.root));
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.src.templates, ['build:templates']);
+  gulp.watch(paths.data.meta,     ['build:templates']);
+  gulp.watch(paths.data.people,   ['build:templates']);
 });
+
+gulp.task('build', ['build:templates']);
 
 gulp.task('dist', ['dist:templates']);
